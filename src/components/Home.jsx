6154ae3be9b76754e8d20f88f4a3ef61cd0a450d
Mahmud sajib <mahmudsajib082@gmail.com>
2023-04-10 02:11:53 +0600

    home hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-// import Lottie from 'lottie-react'
-// import reader from '../assets/reader.json'
-import { ShoppingCartIcon } from '@heroicons/react/24/solid'
+import jobProfile from "../assets/jobman.jpg" 
 import { Link } from 'react-router-dom'
 
 const Home = () => {
@@ -9,39 +7,29 @@ const Home = () => {
     <div className='my-container flex flex-col items-center justify-between lg:flex-row'>
       {/* Text Content */}
       <div className='mb-10 lg:max-w-lg  lg:pr-5 lg:mb-0'>
-        <div className='max-w-xl mb-6 lg:mt-8'>
-        One Step Closer To Your Dream Job
+        <div className='max-w-xl mb-6 lg:mt-8'>  
 
-
-          <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none'>
+          <h2 className='max-w-lg mb-6 font-sans text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none'>
           One Step <br className='hidden md:block' /> Closer To Your{' '}
-            <span className='inline-block text-blue-400'>before he dies</span>
+            <span className='  md:block text-blue-400'> Dream Job</span>
           </h2>
           <p className='text-base text-gray-700 md:text-lg'>
-            Books are a uniquely portable magic. Books serve to show a man that
-            those original thoughts of his aren’t very new after all. The man
-            who does not read good books is no better than the man who can’t.
+          Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
           </p>
         </div>
         <div className='flex flex-col items-center md:flex-row'>
-          <Link to='/books' className='btn md:w-auto md:mr-4'>
+          <Link to='/' className='btn md:w-auto md:mr-4'>
             <div className='inline-flex items-center justify-center w-full h-full'>
-              <p className='mr-3'>Visit Store</p>
-              <ShoppingCartIcon className='w-5 text-gray-100' />
+            <button className="bg-gradient-to-r hidden md:block  from-blue-400 via-indigo-500 to-purple-500 text-white font-bold py-2 px-4 rounded">Star Applying </button>
             </div>
           </Link>
-          <Link
-            to='/about'
-            className='inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-blue-700'
-          >
-            Learn More
-          </Link>
+ 
         </div>
       </div>
       {/* Lottie Animation */}
       <div className='relative lg:w-1/2 '>
         <div className='w-full lg:w-4/5 lg:ml-auto h-56  sm:h-96'>
-          {/* <Lottie animationData={reader} loop={true} > */}
+          <img className='object-cover' src={jobProfile} alt="jobProfile" />
         </div>
       </div>
     </div>
