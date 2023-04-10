46a660eef3a8ca5bfc66c769f8cc2f1168387166
Mahmud sajib <mahmudsajib082@gmail.com>
2023-04-11 02:38:48 +0600

    show all button added

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,16 +5,37 @@ import Feature from './Feature'
 export default function Features() {
 
   const [features,setFeatures] = useState([])
+  const [show,setshowAll] = useState(false)
+
+  const showALLdata =()=>{
+    setshowAll(true)
+  }
   useEffect(()=>{
     fetch('data.json')
     .then(res => res.json())
     .then(data => setFeatures(data))
   },[])
   return (
+    <>
     <div className='grid gap-4 md:grid-cols-2 text-center mx-auto'>
     {
-      features.map(feature => <Feature key={feature.id} feature={feature}/>)
+      features.slice(0,show? 8 :4).map(feature => <Feature key={feature.id} feature={feature}/>)
     }
+   
     </div>
+     
+  {
+    !show && (
+     <div className='text-center'>
+     <span className='inline-block' onClick={showALLdata}>
+        <button className="block mx-auto mt-5 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded ">
+            Show All
+          </button>
+    </span>
+     </div>
+    )
+  }
+  
+    </>  
   )
 }
